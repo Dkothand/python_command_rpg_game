712871651e95f90f88899d3564165850969fe5fc
Dkothand <dkothand@gmail.com>
2020-09-02 20:25:14 -0400

    moved methods pertaining to user action to actions.py UserActions class

--- user_input.py
+++ user_input.py
@@ -18,20 +18,6 @@ class UserInput:
         is_valid = True
         return is_valid 
         
-    def _validate_user_action(self, action):
-        valid_input = UserCommands.options()
-        is_valid = False
-
-        if action == '':
-            print("Not a valid input")
-            return is_valid
-
-        user_input = action.strip().lower()[0]
-        if user_input in valid_input:
-            is_valid = True
-            return is_valid
-        print("Not a valid input")
-
     def get_player_name(self):
         ''' Gets player name from user and validates '''
         while True:
@@ -39,9 +25,3 @@ class UserInput:
             if self._validate_name(name):
                 return name
 
-    def get_user_action(self):
-        ''' Gets input from user and validates '''
-        while True:
-            user_input = input(constants.ACTIONS_PROMPT)
-            if self._validate_user_action(user_input):
-                return user_input            
