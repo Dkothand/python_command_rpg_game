206e25dbe8d9c22d69a9a2d133e212d6ddfcb0c9
Dkothand <dkothand@gmail.com>
2020-09-02 20:29:25 -0400

    Update: refactored Game class to handle UserAction response
    UserActions are for top level menu, Game class will take response from
    UserAction and process command appropriately. This was to avoid having
    to pass player and enemy objects around to different classes and keep
    the game logic centralized.

--- game.py
+++ game.py
@@ -1,3 +1,4 @@
+from battle import Battle
 from hero import Hero
 from enemy import Enemy
 from user_input import UserInput
@@ -17,11 +18,26 @@ class Game:
     def _start_message(self):
         print(constants.GAME_START)
 
-    def _update_game_state(self):
-        self.is_game_running = self.user_actions.toggle_game_state
+    def _handle_menu_action_response(self, res):
+        if res == 'fight':
+            self._create_enemy()
+            battle = Battle(self.player, self.enemy)
+            result = battle.fight()
+            self._check_victory(result)
+        elif res == 'character':
+            print(self.player.__repr__())
+        elif res == 'exit':
+            self._end_game()
 
     def _create_characters(self, user_name):
         self.player = Hero(user_name)
+
+    def _create_enemy(self):
+        self.enemy = Enemy()
+
+    def _check_victory(self, outcome):
+        if outcome == "lose":
+            self._end_game()
 
     def _load_user_actions(self):
         self.user_actions = UserActions(self.player)
@@ -31,14 +47,17 @@ class Game:
         self._create_characters(player_name)
         self._load_user_actions()
 
+    def _end_game(self):
+        self.is_game_running = False
+
     def execute(self, action):
-        self.user_actions.execute_action(action)
-        self._update_game_state()
+        res = self.user_actions.execute_action(action)
+        self._handle_menu_action_response(res)
 
     def run(self):
         self._initalize_game()
         while self.is_game_running:
-            command = self.user_input.get_user_action()
+            command = self.user_actions.get_user_action()
             self.execute(command)
         if self.player.hp == 0:
             print(constants.GAME_OVER_LOSE)
