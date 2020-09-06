59da9eddea91f4e3f03a4b47356173533f7b51d6
Deva <dkothand@gmail.com>
2020-09-06 16:34:00 -0400

    created new BattleCommands class to store battle commands. Moved run into BattleCommands

--- user_commands.py
+++ user_commands.py
@@ -3,10 +3,18 @@ from enum import Enum
 
 class UserCommands(Enum):
     fight = 'f'
-    run = 'r'
     quit = 'q'
     character = 'c'
 
     @classmethod
     def options(cls):
         return tuple(key.value for key in UserCommands)
+
+
+class BattleCommands(Enum):
+    attack = 'a'
+    run = 'r'
+
+    @classmethod
+    def options(cls):
+        return tuple(key.value for key in BattleCommands)
